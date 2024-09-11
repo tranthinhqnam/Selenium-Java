527216e52b2cb3324737d54b172afe7475bd6cb2
tranthinhqnam <tranthinhqnam@gmail.com>
2024-09-11 21:46:13 +0700

    Complete DropdownPage

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public LoginPage clickFormAuthentication() {
+        clickLink("Form Authentication");
+        return new LoginPage(driver);
+    }
+
+    public DropdownPage clickDropdown() {
+        clickLink("Dropdown");
+        return new DropdownPage(driver);
+    }
+    public HoversPage clickHovers(){
+        clickLink("Hovers");
+        return new HoversPage(driver);
+    }
+    public KeyPressesPage clickKeyPresses(){
+        clickLink("Key Presses");
+        return new KeyPressesPage(driver);
+    }
+    private void clickLink(String linkText) {
+        driver.findElement(By.linkText(linkText)).click();
+    }
+}
